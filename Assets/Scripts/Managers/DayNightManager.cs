0253ad427eda9ac0f4d8209b0efeb9c0df5d50d3
Yoni-P <yonipushett@gmail.com>
2025-08-02 23:00:09 +0300

    Change backround color

--- Assets/Scripts/Managers/DayNightManager.cs
+++ Assets/Scripts/Managers/DayNightManager.cs
@@ -6,13 +6,17 @@ public class DayNightManager : MonoBehaviour
 {
     [SerializeField] private RotatingPlanet sun;
     [SerializeField] private RotatingPlanet moon;
+    
+    [SerializeField] private Material skyMaterial; // Material to change the sky color
 
     private RotatingPlanet[] _planets;
+    private Light[] _globalLights;
     private int _currentPlanetIndex = 0;
 
     private void Start()
     {
         _planets = new RotatingPlanet[] { sun, moon };  
+        _globalLights = new Light[] { sun.GetComponentInChildren<Light>(), moon.GetComponentInChildren<Light>() };
         StartCoroutine(DayNightCycle(_planets[_currentPlanetIndex]));
     }
 
@@ -25,6 +29,24 @@ public class DayNightManager : MonoBehaviour
             yield return null; // Wait for the next frame
         }
         StartCoroutine(DayNightCycle(GetNextPlanet()));
+    }
+    
+    private void Update()
+    {
+        if (skyMaterial != null)
+        {
+            // Get the value of all light sources in the scene
+            var intensity = 0f;
+            foreach (var light in _globalLights)
+            {
+                if (light != null)
+                {
+                    intensity += light.intensity;
+                }
+            }
+            intensity = Mathf.Clamp(intensity, 0f, 1f); // Ensure intensity is between 0 and 1
+            skyMaterial.SetFloat("_Blend", 1 - intensity);
+        }
     }
 
     private RotatingPlanet GetNextPlanet()
