812865f3f2c747e88aaf66acf0b5b1e2f8d80320
Yoni-P <yonipushett@gmail.com>
2025-08-03 15:43:34 +0300

    Add day night music

--- Assets/Scripts/Managers/DayNightManager.cs
+++ Assets/Scripts/Managers/DayNightManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using FMODUnity;
 using UnityEngine;
 
 public class DayNightManager : MonoBehaviour
@@ -8,6 +9,8 @@ public class DayNightManager : MonoBehaviour
     [SerializeField] private RotatingPlanet moon;
     
     [SerializeField] private Material skyMaterial; // Material to change the sky color
+    
+    [SerializeField] private StudioEventEmitter dayNightMusicEmitter; // FMOD event for day/night music
 
     private RotatingPlanet[] _planets;
     private Light[] _globalLights;
@@ -46,6 +49,24 @@ public class DayNightManager : MonoBehaviour
             }
             intensity = Mathf.Clamp(intensity, 0f, 1f); // Ensure intensity is between 0 and 1
             skyMaterial.SetFloat("_Blend", 1 - intensity);
+        }
+        
+        if (dayNightMusicEmitter != null && dayNightMusicEmitter.IsPlaying())
+        {
+            var day = 0.5f;
+            dayNightMusicEmitter.EventInstance.getParameterByName("day", out var dayParameter);
+            if (sun.isActiveAndEnabled)
+            {
+                var sunProg = sun.GetProgress() <= 0.5f ? sun.GetProgress() : 1f - sun.GetProgress();
+                day += sunProg;
+            if (moon.isActiveAndEnabled)
+            {
+                var moonprog = moon.GetProgress() <= 0.5f ? moon.GetProgress() : 1f - moon.GetProgress();
+                day -= moonprog;
+            }
+            day = Mathf.Clamp(day, 0f, 1f); // Ensure day parameter is between 0 and 1
+            dayNightMusicEmitter.EventInstance.setParameterByName("day", day);
+            }
         }
     }
 
